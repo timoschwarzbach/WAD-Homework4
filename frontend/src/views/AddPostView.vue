2442626436bade4df8ec8ab1993e02481e14ff9e
Louis Goerlt <louis.goerlt@gmail.com>
2025-12-13 00:01:05 +0200

    add post design

--- frontend/src/views/AddPostView.vue
+++ frontend/src/views/AddPostView.vue
@@ -25,14 +25,70 @@ export default {
 
 <template>
   <div class="post_input">
-    <label for="post_input">Post body</label>
-    <textarea
-      name="fbody"
-      id="post_input"
-      placeholder="textarea"
-      required
-      v-model="postDesc"
-    ></textarea>
+    <label for="post_input">Add Post</label>
+    <div class="input_area">
+      <label for="post_input">Body</label>
+      <textarea
+        name="fbody"
+        id="post_input"
+        placeholder="textarea"
+        required
+        v-model="postDesc"
+      ></textarea>
+    </div>
+    <input type="submit" id="submit_button" @click="addPost" value="Add" />
   </div>
-  <input type="submit" id="submit_button" @click="addPost" value="Add" />
 </template>
+
+<style>
+.post_input {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #ecf1e2;
+  border-radius: 12px;
+  max-width: 300px;
+  padding: 4px;
+  margin: 0 auto;
+}
+
+.post_input > label {
+  padding: 10px 0;
+}
+
+.input_area {
+  flex-direction: row;
+  vertical-align: middle;
+  margin-left: auto;
+  margin-right: 10px;
+  display: flex;
+  align-items: center;
+}
+
+.input_area > label {
+  padding: 0 40px;
+}
+
+textarea {
+  background-color: ghostwhite;
+  border: none;
+  border-radius: 12px;
+  text-align: center;
+  margin: 5px;
+  padding: 10px 0;
+  resize: none;
+  width: 170px;
+  height: 12px;
+  overflow: hidden;
+}
+
+input {
+  background-color: cornflowerblue;
+  border-radius: 12px;
+  border: none;
+  padding: 6px 12px;
+  font-size: 16px;
+  margin: 10px;
+}
+</style>
