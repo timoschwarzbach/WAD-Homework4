d51d32d8e45cc9c2d005b47c3c3cf017c92ff5c0
Louis Goerlt <louis.goerlt@gmail.com>
2025-12-12 23:44:21 +0200

    post edit design

--- frontend/src/views/UpdatePostView.vue
+++ frontend/src/views/UpdatePostView.vue
@@ -1,15 +1,31 @@
 <template>
   <div class="post_input">
-    <label for="post_input">Post body</label>
-    <textarea
-      name="fbody"
-      id="post_input"
-      placeholder="textarea"
-      required
-      v-model="postBody"
-    ></textarea>
+    <label for="post_input">A Post</label>
+    <div class="input_area">
+      <label for="post_input">Body</label>
+      <textarea
+        name="fbody"
+        id="post_input"
+        placeholder="textarea"
+        required
+        v-model="postBody"
+      ></textarea>
+    </div>
+    <div class="row">
+      <input
+        type="submit"
+        id="submit_button"
+        @click="updatePost"
+        value="Update"
+      />
+      <input
+        type="submit"
+        id="submit_button"
+        @click="deletePost"
+        value="Delete"
+      />
+    </div>
   </div>
-  <input type="submit" id="submit_button" @click="updatePost" value="Update" />
 </template>
 
 <script>
@@ -51,3 +67,56 @@ export default {
   },
 };
 </script>
+
+<style>
+.post_input {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #ecf1e2;
+  border-radius: 12px;
+  max-width: 300px;
+  padding: 4px;
+  margin: 0 auto;
+}
+
+.post_input > label {
+  padding: 10px 0;
+}
+
+.input_area {
+  flex-direction: row;
+  vertical-align: middle;
+  margin-left: auto;
+  margin-right: 10px;
+  display: flex;
+  align-items: center;
+}
+
+.input_area > label {
+  padding: 0 40px;
+}
+
+textarea {
+  background-color: ghostwhite;
+  border: none;
+  border-radius: 12px;
+  text-align: center;
+  margin: 5px;
+  padding: 10px 0;
+  resize: none;
+  width: 170px;
+  height: 12px;
+  overflow: hidden;
+}
+
+input {
+  background-color: cornflowerblue;
+  border-radius: 12px;
+  border: none;
+  padding: 6px 12px;
+  font-size: 16px;
+  margin: 10px;
+}
+</style>
