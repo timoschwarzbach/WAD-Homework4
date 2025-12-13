f22f09fb3476262e0bcd417f7dfa458a383bb8e3
Louis Goerlt <louis.goerlt@gmail.com>
2025-12-13 15:48:45 +0200

    update contacts

--- frontend/src/views/ContactsView.vue
+++ frontend/src/views/ContactsView.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Contacts</h1>
+    <p>Henry Vogel</p>
+    <p>Louis Goerlt</p>
+    <p>Timo Schwarzbach</p>
   </div>
 </template>
 
